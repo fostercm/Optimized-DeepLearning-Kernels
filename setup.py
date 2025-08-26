91821000f70d2076aa07f0fb03f393584b63bc70
Cole Foster <cmf290@miami.edu>
2025-08-26 13:19:05 -0400

    Build process

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+ext_modules=[
+    CUDAExtension(
+        name="fastkern._add",
+        sources=[
+            "fastkern/add/add.cu"
+        ]
+    )
+]
+
 setup(
-    name='test_extension',
-    ext_modules=[
-        CUDAExtension(
-            name='test_extension',
-            sources=['addition/addition.cu'],
-        )
-    ],
+    name='fastkern',
+    version='0.1',
+    packages=find_packages(),
+    ext_modules=ext_modules,
     cmdclass={
         'build_ext': BuildExtension
-    }
+    },
+    zip_safe=False
 )
