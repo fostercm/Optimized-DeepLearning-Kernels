18d665c07d7701f60cd492416bfececca5a8b810
Cole Foster <cmf290@miami.edu>
2025-08-26 13:24:17 -0400

    Improved testing with pytest

---
+++ tests/test_add.py
@@ -0,0 +1,18 @@
+from typing import Literal
+import torch
+import fastkern as fk
+import pytest
+
+
+@pytest.mark.parametrize("shape", [
+    (1024, 768),     # residuals
+    (1024, 3072),   # MLP up/down
+    (1024, 2304),   # QKV projection bias
+])
+
+def test_add(shape: Literal[1024]):
+    a = torch.randn(shape, dtype=torch.float32, device='cuda')
+    b = torch.randn(shape, dtype=torch.float32, device='cuda')
+    c = fk.add(a, b)
+    d = a + b
+    assert (c == d).all()
