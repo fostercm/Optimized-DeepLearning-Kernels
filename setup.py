f94d30214e8a4d23920434164bd46d27b8898fbf
Cole Foster <cmf290@miami.edu>
2025-09-08 11:22:42 -0400

    architecture-specific compilation

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import os
+
+os.environ['TORCH_CUDA_ARCH_LIST'] = '8.6'
 
 ext_modules=[
     CUDAExtension(
         name="fastkern._add",
         sources=[
             "fastkern/add/add.cu"
+        ]
+    ),
+    CUDAExtension(
+        name="fastkern._mult",
+        sources=[
+            "fastkern/mult/mult.cu"
         ]
     )
 ]
@@ -16,7 +25,7 @@ setup(
     packages=find_packages(),
     ext_modules=ext_modules,
     cmdclass={
-        'build_ext': BuildExtension
+        'build_ext': BuildExtension.with_options(use_ninja=True)
     },
     zip_safe=False
 )
